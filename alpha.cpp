b2ed47d40012ffb18d1cee000cc24c51c85f03e4
mr360 <qd0097@gmail.com>
2018-04-14 03:28:03 +1000

    Comment out cout

--- alpha.cpp
+++ alpha.cpp
@@ -1,4 +1,4 @@
 int main() {
-      std::cout << "Helloworld, this is John and you are in Alpha cpp" << std::endl;
+      //std::cout << "Helloworld, this is John and you are in Alpha cpp" << std::endl;
       return 0;
   }
